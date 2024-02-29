096c27a08f85a205426f38955b13836d39da9e28
Ayman Velani <secondayman@gmail.com>
2024-02-29 17:41:24 +0530

    branch M7

--- server.js
+++ server.js
@@ -1,12 +1,39 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+dotenv.config(); 
 
-app.get('/ping',(req,res)=>{
-    res.send('pong')
-})
+const mongoose = require('mongoose');
 
-app.listen(3000,()=>{
-    console.log('port 3000')
-})
+let connectionStatus = 'Disconnected';
 
-module.exports = app
+const startDatabase = async () => {
+    try {
+        await mongoose.connect(process.env.URI);
+        connectionStatus = 'Connected';
+    } catch (err) {
+        console.error("Failed to connect to MongoDB:", err); 
+        connectionStatus = 'Failed';
+    }
+};
+
+app.get('/ping', (req, res) => {
+    res.send('pong');
+});
+
+app.get('/', async (req, res) => {
+    
+    try {
+        await mongoose.connect(con) 
+        res.send(connectionStatus);
+    } catch (err) {
+        console.error("Failed to get database connection status:", err);
+    }
+});
+
+app.listen(4000, () => {
+    startDatabase();
+    console.log('Listening on port 4000');
+});
+
+module.exports = app;
